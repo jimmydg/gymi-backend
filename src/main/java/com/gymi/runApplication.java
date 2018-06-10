59d5148cdd34a23bf2177a2afc7ee8ca166e1323
milansteenwinkel <milanosie@live.nl>
2018-06-10 18:04:32 +0200

    add deploy settings

---
+++ src/main/java/com/gymi/runApplication.java
@@ -0,0 +1,14 @@
+package com.gymi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class runApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(runApplication.class, args);
+	}
+}
