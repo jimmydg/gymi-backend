ff62b914be067d2821326ebc1d86538b04d3872f
milansteenwinkel <milanosie@live.nl>
2018-06-17 14:09:37 +0200

    Add new application

--- src/main/java/com/gymi/Application.java
+++ src/main/java/com/gymi/Application.java
@@ -2,11 +2,18 @@ package com.gymi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
