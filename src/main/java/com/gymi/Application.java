2e4ea173f4511fe588086ce95c6ba3729916e708
milansteenwinkel <milanosie@live.nl>
2018-06-17 14:43:48 +0200

    remove some stuff

--- src/main/java/com/gymi/Application.java
+++ src/main/java/com/gymi/Application.java
@@ -1,23 +1,23 @@
-//package com.gymi;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//
-//@SpringBootApplication
-//@EnableJpaAuditing
-//public class Application extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(Application.class);
-//	}
-//
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(Application.class, args);
-//	}
-//}
-//
+package com.gymi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
